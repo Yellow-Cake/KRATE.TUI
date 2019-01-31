7b8959f2c9e703992e841ca7dd1a42fc524a2ce9
Tayab Soomro <isoomro.t@gmail.com>
2019-01-30 20:21:12 -0600

    add POST method

--- RestClient/Utility.cs
+++ RestClient/Utility.cs
@@ -1,17 +1,48 @@
 using System.Net;
 using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 
-namespace StandaloneExample.RestClient {
-  class Utility {
-    public static string Get(string uri){
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+namespace KrateTUI.RestClient {
+    class Utility {
 
-        using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-        using(Stream stream = response.GetResponseStream())
-        using(StreamReader reader = new StreamReader(stream)){
-            return reader.ReadToEnd();
+        private static readonly HttpClient client = new HttpClient();
+
+        public static JObject Get(string uri){
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using(Stream stream = response.GetResponseStream())
+            using(StreamReader reader = new StreamReader(stream)){
+                    return JObject.Parse(reader.ReadToEnd());
+            }
+        }
+
+
+        public static string Post(string uri)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://www.example.com/recepticle.aspx");
+
+            var postData = "thing1=hello";
+            postData += "&amp;amp;amp;amp;amp;thing2=world";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responseString;
         }
     }
-  }
 }
